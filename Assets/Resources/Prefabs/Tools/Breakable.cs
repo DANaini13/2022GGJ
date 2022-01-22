8b4f91d64ba13bd0391cdd45584d484d0e89d61b
zeyong shan <danaini13@gmail.com>
2022-01-22 23:29:22 +0800

    对一个走路粒子

--- Assets/Resources/Prefabs/Tools/Breakable.cs
+++ Assets/Resources/Prefabs/Tools/Breakable.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using UnityEditor.iOS;
 using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject explod_cube = null;
+
+    public void Awake()
     {
-        
+        explod_cube = Resources.Load("Prefabs/DoNotChange/VFX_Cube_Clap") as GameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Break()
     {
-        
+        var child_count = transform.childCount;
+        for (int i = 0; i < child_count; i++)
+        {
+            var child = transform.GetChild(i);
+            var pos = child.position;
+            var fx = Instantiate(explod_cube, transform).GetComponent<Explod>();
+            fx.transform.position = pos;
+            GameObject.Destroy(child.gameObject);
+        }
     }
 }
