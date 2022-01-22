9ddea93877a371895741b833ade7dda5ac195ae1
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2022-01-23 05:18:12 +0800

    道具旋转和地板

--- Assets/Arts/PathGenerator.cs
+++ Assets/Arts/PathGenerator.cs
@@ -7,12 +7,15 @@ public class PathGenerator : MonoBehaviour
     public bool isActive = false;
     public int count = 20;
     public Transform tilePrefab;
+    public int probability = 100;
 
     void Awake()
     {
         if (!isActive) return;
         for (int i = 0; i < count; i++)
         {
+            if (Random.Range(0, 100) > probability) continue;
+
             Transform prefab = Instantiate(tilePrefab);
             prefab.gameObject.SetActive(true);
             prefab.SetParent(this.transform);
