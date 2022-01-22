52953d1a0caf65bafadc1b10b899b9f018398169
zeyong shan <danaini13@gmail.com>
2022-01-22 13:59:46 +0800

    没有俯仰角的面向主摄像机

---
+++ Assets/Scripts/CameraFace.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFace : MonoBehaviour
+{
+    private Camera main_camera;
+    void Start()
+    {
+        main_camera = Camera.main;
+    }
+
+    void Update()
+    {
+        var temp = transform.eulerAngles;
+        transform.LookAt(main_camera.transform);
+        var current_el = transform.eulerAngles;
+        current_el.x = temp.x;
+        current_el.z = temp.z;
+        transform.eulerAngles = current_el;
+    }
+}
