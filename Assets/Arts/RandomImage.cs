7492948cee72b240140a1b6be140099da0a5c84c
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2022-01-22 19:15:19 +0800

    测试一下场景

--- Assets/Arts/RandomImage.cs
+++ Assets/Arts/RandomImage.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class RandomImage : MonoBehaviour
 {
-    public Sprite[] sprites;
+    public MeshRenderer[] mrs;
+    public Texture[] texs;
+
 
     void Awake()
     {
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        var block = new MaterialPropertyBlock();
+        block.SetTexture("_PropTex", texs[Random.Range(0, texs.Length)]);
+        for (int i = 0; i < mrs.Length; i++)
+            mrs[i].SetPropertyBlock(block);
     }
 }
