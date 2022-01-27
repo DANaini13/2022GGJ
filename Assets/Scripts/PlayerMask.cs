fc3e6d1dffc87aca1a84745af3e0fe26f639ee2d
YaNgYu <75065276+YaN9Yu96@users.noreply.github.com>
2022-01-27 14:51:47 +0800

    开关灯改为对方只持续5秒

--- Assets/Scripts/PlayerMask.cs
+++ Assets/Scripts/PlayerMask.cs
@@ -13,9 +13,43 @@ public class PlayerMask : MonoBehaviour
         instance = this;
         trans = GetComponent<RectTransform>();
     }
-    
+
     public Transform p1_trans;
     public Transform p2_trans;
+
+    void Update()
+    {
+        if (p1_black.isPlaying)
+        {
+            p1_timer -= Time.deltaTime;
+            if (p1_timer <= 0f)
+                p1_black.Stop();
+        }
+
+        if (p2_black.isPlaying)
+        {
+            p2_timer -= Time.deltaTime;
+            if (p2_timer <= 0f)
+                p2_black.Stop();
+        }
+    }
+
+    float p1_timer, p2_timer;
+    public void LightP1()
+    {
+        p1_black.Play();
+        p2_black.Stop();
+        p1_timer = 5f;
+        p2_timer = 0f;
+    }
+
+    public void LightP2()
+    {
+        p2_black.Play();
+        p1_black.Stop();
+        p2_timer = 5f;
+        p1_timer = 0f;
+    }
 
     public void ChangeLight()
     {
@@ -52,8 +86,8 @@ public class PlayerMask : MonoBehaviour
         }
         else
         {
-             CameraUtil.GetCameraById(0).depth = 0;
-             CameraUtil.GetCameraById(1).depth = 1;
+            CameraUtil.GetCameraById(0).depth = 0;
+            CameraUtil.GetCameraById(1).depth = 1;
         }
     }
 }
